59c2f5b40303ec9953ce61bdb78e759b49be958f
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-10-25 08:07:13 -0500

    Change decimal permission input to readonly

--- src/components/permission-calculator.svelte
+++ src/components/permission-calculator.svelte
@@ -93,6 +93,7 @@
                 bind:value="{decimalPermission}"
                 minlength="3"
                 maxlength="3"
+                readonly
                 />
                 <button type="button" on:click={() => copyValueToClipboard('numeric-perm')} title="Copy">
                     <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
